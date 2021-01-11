c715f29ef1fdd616bc12630bd9d686f25dad704e
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-01-11 13:28:17 +0200

    fixes

--- js/task.js
+++ js/task.js
@@ -1,43 +1,49 @@
-import users from "./users.js";
+import users from "./users.js"
+import usersArray from "./users.js"
 
-const getUserNames = users => users.map(user => user.name);
-console.log(getUserNames(users));
+const getUserNames = users => users.map(user => user.name)
+console.log(getUserNames(usersArray));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
 const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
-console.log(getUsersWithEyeColor(users, 'blue'));
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
-const getUsersWithGender = (users, gender) => users.filter(user => user.gender === gender);
-console.log(getUsersWithGender(users, 'male'));
+const getUsersWithGender = (users, gender) => users
+    .filter(user => user.gender === gender)
+    .map(user => user.name);
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
 const getInactiveUsers = users => users.filter(user => !user.isActive);
-console.log(getInactiveUsers(users));
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
 const getUserWithEmail = (users, email) => users.find(user => user.email === email);
-console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
-console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
 
-const getUsersWithAge = (users, min, max) => users.filter(user => user.age >= min && user.age <= max);
-console.log(getUsersWithAge(users, 20, 30));
-console.log(getUsersWithAge(users, 30, 40));
+const getUsersWithAge = (users, min, max) => users.filter(({ age }) => age >= min && age <= max);
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+console.log(getUsersWithAge(users, 30, 40)); // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
-const calculateTotalBalance = users => users.reduce((acc, user) => acc + user.balance, 0);
-console.log(calculateTotalBalance(users));
+const calculateTotalBalance = users => users.reduce((totalBalance, { balance }) => totalBalance + balance, 0);
+console.log(calculateTotalBalance(users)); // 20916
 
-const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
-console.log(getUsersWithFriend(users, 'Briana Decker'));
-console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+const getUsersWithFriend = (users, friendName) => users
+    .filter(({ friends }) => friends.includes(friendName))
+    .map(user => user.name);
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
 const getNamesSortedByFriendsCount = users => {
-    return users
-        .sort((a, b) => a.friends.length - b.friends.length)
-        .map(user => user.name);
+    const friendsSort = function(a, b) {return a.friends.length - b.friends.length}
+    return users.sort(friendsSort).map(user => user.name);
 };
 console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
-const getSortedUniqueSkills = users => {
-    return users
-        .reduce((totalSkills, user) => { totalSkills.push(...user.skills); return totalSkills }, [])
-        .reduce((uniqueSkills, skill) => { if (!uniqueSkills.includes(skill)) uniqueSkills.push(skill); return uniqueSkills }, [])
-        .sort();
-}
+const getSortedUniqueSkills = users => users
+    .reduce((allSkills, { skills }) => {
+        const uniqueSkills = skills.filter(skill => !allSkills.includes(skill))
+        return [...allSkills, ...uniqueSkills];
+    }, [])
+    .sort();
 console.log(getSortedUniqueSkills(users));
